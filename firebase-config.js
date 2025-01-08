18bba8d40c665eff1784ad7b988fb8630afd94c8
seojunLee203 <115590672+seojunLee203@users.noreply.github.com>
2025-01-07 20:47:53 -1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDfgIompTwM8HOGJtEZCVivP3bakNCDbpQ",
+    authDomain: "cyber-trail.firebaseapp.com",
+    projectId: "cyber-trail",
+    storageBucket: "cyber-trail.appspot.com",
+    messagingSenderId: "10375411657",
+    appId: "1:10375411657:web:755e620278b4fd68ad5a18",
+    measurementId: "G-J9Y3TQMH5T"
+};
 
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.getAnalytics(app);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+export { auth, db };
